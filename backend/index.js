2591a6aadd13bdffb0f43715f11cf3684f9b752d
Trevor Debardeleben <trevordebardeleben@Trevors-MacBook-Pro.local>
2020-03-22 20:51:13 -0500

    Allow cors http DO environment

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
 import './utils/config';
 import express from 'express';
 import Arkansas from './states/Arkansas';
+import cors from 'cors';
 // import './utils/cron';
 
 const app = express();
 
-app.get('/AR', async (req, res, next) => {
+var whitelist = [
+  'http://localhost:1234',
+  'https://167.172.224.227',
+  'https://167.172.224.227:1234',
+  'http://167.172.224.227:1234',
+];
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+app.get('/api/AR', async (req, res, next) => {
   const AR = new Arkansas();
   res.json(await AR.getData());
 });
