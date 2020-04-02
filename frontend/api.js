639643dc882f68563ac9003627e68ee6d6ad93cd
trevordebard <trevordebard@gmail.com>
2020-04-02 03:24:53 +0000

    Use correct API for dev/prod

--- frontend/api.js
+++ frontend/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_ENDPOINT = process.env.REACT_APP_API_ENDPOINT || 'http://167.172.224.227:2093/api';
+const API_ENDPOINT = process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_ENDPOINT : '';
 
 export async function getData(state) {
-  const { data } = await axios.get(`${API_ENDPOINT}/${state}`);
+  const { data } = await axios.get(`${API_ENDPOINT}/api/${state}`);
   return data;
 }
